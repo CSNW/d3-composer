a42f813c0e77854dc611df03890da6d28738b421
Tim Hall <tim.hall.engr@gmail.com>
2018-09-10 18:03:02 -0400

    Update example

--- packages/example/src/index.js
+++ packages/example/src/index.js
@@ -131,7 +131,7 @@ function barsChart(selection, props) {
       data,
       xScale,
       yScale,
-      seriesStyle: { fill: 'green' },
+      seriesStyle: { color: 'green' },
       transition
     });
   });
